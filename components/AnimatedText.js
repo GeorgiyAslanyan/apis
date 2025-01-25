2e1834e8ea02f7ae7be8e585254f182b02757d2b
GeorgiyAslanyan <13jeff07@gmail.com>
2025-01-26 00:35:44 +0500

    whileinview fix

--- components/AnimatedText.js
+++ components/AnimatedText.js
@@ -1,13 +1,24 @@
 import { motion } from 'framer-motion';
+import { useRef } from 'react';
 
 const AnimatedText = ({ text }) => {
   const words = text.split(' ');
+  const scrollRef = useRef(null)
 
   return (
-    <motion.div 
+    <motion.div ref={scrollRef}
       className='flex flex-wrap max-w-full' 
+      initial="hidden"
       whileInView="visible"
-      viewport={{ once: true, amount: 0.5 }} // Анимация сработает только один раз, когда элемент будет виден на 50%
+      variants={{
+        hidden: {}, // Начальное состояние (невидимо)
+        visible: { // Состояние, когда элемент виден
+          transition: {
+            staggerChildren: 0.1, // Задержка между анимацией каждого слова
+          },
+        },
+      }}
+      viewport={{ once: true, amount: 0.8}} // Анимация сработает только один раз, когда элемент будет виден на 50%
     >
       {words.map((word, wordIndex) => (
         <motion.span
