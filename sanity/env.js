3b4dcd39e347ebc1a42c68651097786906907b1e
GeorgiyAslanyan <13jeff07@gmail.com>
2025-01-31 00:45:24 +0500

    upd env

--- sanity/env.js
+++ sanity/env.js
@@ -1,6 +1,6 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '1'
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const dataset = "production";
+export const projectId = "jbrgprzz";
 export const useCdn = false
